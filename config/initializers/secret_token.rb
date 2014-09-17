6e56468f49247e63603277f07ba0020310a20c8b
Kobi <pingkobi@gmail.com>
2014-09-17 12:11:24 -0700

    First commit.  generate about & contact controller --no-test-framework.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Booktour::Application.config.secret_key_base = '3f5c86d88cfa3e17dc6304f2050071a02edeebe449ffc80c4877fb60aae0cb8aa607363ac5453021e2c5a8f2e10b355fc80f76bd6926e5aef3d31af28bf78a53'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Booktour::Application.config.session_store :cookie_store, key: '_booktour_session'
