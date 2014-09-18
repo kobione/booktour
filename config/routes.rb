77b36b1e8d8b57861fa24690eec80013306ed7d6
Kobi <pingkobi@gmail.com>
2014-09-17 20:51:24 -0700

    Add Bootstrap.  Add navbar using Bootstrap.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Booktour::Application.routes.draw do
   resources :listings
 
-  get "static_pages/about"
-  get "static_pages/contact"
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+
+  root "listings#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
