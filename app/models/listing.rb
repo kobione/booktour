8aad887f24bdcedc98560b16cb699d59e232a03f
Kobi <pingkobi@gmail.com>
2014-09-29 15:38:59 -0700

    Correct production.rb file to point to app (NOT localhost:3000, which is only for development.rb) Revert previous testing line.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,11 +1,11 @@
 class Listing < ActiveRecord::Base
 	if Rails.env.development?
-		has_attached_file 	:image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "image_not_found.png"
+		has_attached_file 	:image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "image_not_found.png"
 	else
-		has_attached_file 	:image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "image_not_found.png",
+		has_attached_file 	:image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "image_not_found.png",
 		        			:storage => :dropbox,
 	        				:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
 	        				:path => ":style/:id_:filename"
-	  	#validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+	  	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 	end
 end
