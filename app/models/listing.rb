4daa895fc24ef95c50e03b4be67da5880f40fc97
Kobi <pingkobi@gmail.com>
2014-09-30 00:54:43 -0700

    manually revert by commenting out and deleting lines to get to working version.  Note, less secure.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,11 +1,16 @@
 class Listing < ActiveRecord::Base
-	if Rails.env.development?
-		has_attached_file 	:image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "image_not_found.png"
-	else
-		has_attached_file 	:image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "image_not_found.png",
-		        			:storage => :dropbox,
-	        				:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-	        				:path => ":style/:id_:filename"
-	  	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-	end
+	has_attached_file 	:image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "image_not_found.png",
+						:storage => :dropbox,
+						:dropbox_credentials => Rails.root.join("config/dropbox.yml")
+	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
+	# if Rails.env.development?
+	# 	has_attached_file 	:image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "image_not_found.png"
+	# else
+	# 	has_attached_file 	:image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "image_not_found.png",
+	# 	        			:storage => :dropbox,
+	#         				:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+	#         				:path => ":style/:id_:filename"
+	#   	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+	# end
 end
