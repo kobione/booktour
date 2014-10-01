4d98150e55530cf26102c6ea459441ec666d9030
Kobi <pingkobi@gmail.com>
2014-10-01 10:04:22 -0700

    Working.  Needed to run 'figaro heroku:set -e production' to tell heroku where to find the 'secret' values in the ENV[] hash.  (NOTE: The tutorial says to run 'rake figaro:heroku' but that seems to be deprecated.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,13 +4,13 @@ class Listing < ActiveRecord::Base
 						:dropbox_credentials => Rails.root.join("config/dropbox.yml")
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
-# if Rails.env.development?
-# 	has_attached_file 	:image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "image_not_found.png"
-# else
-# 	has_attached_file 	:image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "image_not_found.png",
-# 	        			:storage => :dropbox,
-#         				:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-#         				:path => ":style/:id_:filename"
-#   	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-# end
+	# if Rails.env.development?
+	# 	has_attached_file 	:image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "image_not_found.png"
+	# else
+	# 	has_attached_file 	:image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "image_not_found.png",
+	# 	        			:storage => :dropbox,
+	#         				:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+	#         				:path => ":style/:id_:filename"
+	#   	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+	# end
 end
