0fa3bc970196927d9609828e142738c640e2c61f
Kobi <pingkobi@gmail.com>
2014-10-01 13:16:18 -0700

    Add image validation line (now required) to development instructions of using images (not dropbox instructions, but still needs validation.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,7 @@
 class Listing < ActiveRecord::Base
 	if Rails.env.development?
 		has_attached_file 	:image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "image_not_found.png"
+		validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 	else
 		has_attached_file 	:image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "image_not_found.png",
 							:storage => :dropbox,
