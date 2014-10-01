c6f0b983401e0c13081ee45d7bb542f99913fef4
Kobi <pingkobi@gmail.com>
2014-10-01 13:45:01 -0700

    Clean up comments. Heroku should be running with Dropbox (if all previous listings were deleted. Can create new listings.)

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -9,14 +9,4 @@ class Listing < ActiveRecord::Base
 							:path => ":style/:id_:filename"
 		validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 	end
-
-	# if Rails.env.development?
-	# 	has_attached_file 	:image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "image_not_found.png"
-	# else
-	# 	has_attached_file 	:image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "image_not_found.png",
-	# 	        			:storage => :dropbox,
-	#         				:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-	#         				:path => ":style/:id_:filename"
-	#   	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-	# end
 end
