ca443b1f081e0d73341005c6a4825d49310e0653
Kobi <pingkobi@gmail.com>
2014-10-02 23:44:12 -0700

    Return mistakenly deleted line.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -6,6 +6,7 @@ class Listing < ActiveRecord::Base
 		has_attached_file 	:image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "image_not_found.png",
 							:storage => :dropbox,
 							:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+							:path => ":style/:id_:filename"
 		validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 	end
 
