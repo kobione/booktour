eee34cf129ef008ee0bc3cedd54dc0ea0bee4bf1
Kobi <pingkobi@gmail.com>
2014-10-08 01:54:49 -0700

    Test resizing of default image to :thumb.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,9 +1,9 @@
 class Listing < ActiveRecord::Base
 	if Rails.env.development?
-		has_attached_file 	:image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "image_not_found.png"
+		has_attached_file 	:image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "image_not_found_:styles.png"
 		validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 	else
-		has_attached_file 	:image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "image_not_found.png",
+		has_attached_file 	:image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "image_not_found_:styles.png",
 							:storage => :dropbox,
 							:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
 							:path => ":style/:id_:filename"
